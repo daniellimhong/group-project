034589361137419c5d1bf7a9395544d806cd798c
iamnitinmisra <nitin.misra84@gmail.com>
2019-08-29 15:08:21 -0700

    fixing the merge

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,20 +1,29 @@
-import React, {Component} from 'react'
-import {NavLink} from 'react-router-dom';
-import Login from '../Login/Login';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import Login from "../Login/Login";
 import "./NavBar.scss";
 
-export default function NavBar(props){
-    return(
-        <div>
-            <NavLink exact to="/ForSale" activeClassName="active">
-                Cars For Sale
-            </NavLink>
-            <NavLink exact to="/SellCar" activeClassName="active">
-                Sell Your Car
-            </NavLink>
-            <div>
-                <Login />
-            </div>
-        </div>
-    )
+export default function NavBar(props) {
+  return (
+    <div>
+      <div>
+        <NavLink exact to="/ForSale" activeClassName="active">
+          Cars For Sale
+        </NavLink>
+      </div>
+      <div>
+        <NavLink exact to="/SellCar" activeClassName="active">
+          Sell Your Car
+        </NavLink>
+      </div>
+      <div>
+          <NavLink to="/NewListing" activeClassName="active">
+              New Listing
+          </NavLink>
+      </div>
+      <div>
+        <Login />
+      </div>
+    </div>
+  );
 }
