2ec23149710bef46c92694d5fe87a2fb0d5a5902
iamnitinmisra <nitin.misra84@gmail.com>
2019-08-29 11:56:45 -0700

    updated App.js with NewListing route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './Components/Login/Login';
 import Home from './Components/Home/Home';
 import SearchCar from './Components/SearchCar/SearchCar';
 import Register from './Components/Register/Register';
+import NewListing from './Components/NewListing/NewListing'
 import SellCar from "./Components/SellCar/SellCar";
 import ForSale from './Components/ForSale/ForSale';
 import YourListing from './Components/YourListing/YourListing';
@@ -23,12 +24,14 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home}/> 
           {/* {/* <Route path="/SearchCar" component={SearchCar}/> */}
-          <Route path="/Register" component={Register}/>
-          {/* <Route path="/SellCar" component={SellCar}/> */}
-          {/* <Route path="/ForSale" component={ForSale}/> */}
-          {/* <Route path="/YourListing" component={YourListing}/> */}
-          {/* <Route path="/Contact" component={Contact}/> */}
-          {/* <Route path="Profile" component={Profile}/> */} */} */}
+          <Route path="/register" component={Register}/>
+          <Route path="/newlisting" component={NewListing}/>
+          {/* <Route path="/sellcar" component={SellCar}/> */}
+          {/* <Route path="/forsale" component={ForSale}/> */}
+          {/* <Route path="/yourlisting" component={YourListing}/> */}
+          {/* <Route path="/contact" component={Contact}/> */}
+          {/* <Route path="profile" component={Profile}/> */}
+           
         </Switch>
       </div>
 
