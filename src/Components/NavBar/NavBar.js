3beebade55f1ed3932166b6e046c1b4bb021ec94
thomas brown <thomasbrown9101@gmail.com>
2019-09-03 15:52:16 -0700

    updated ternary

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,45 +1,56 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import Login from "../Login/Login";
 import "./NavBar.scss";
-import {connect } from 'react-redux';
-import {getUser} from '../../redux/reducer';
+import { connect } from "react-redux";
+import { getUser } from "../../redux/reducer";
 
- class NavBar extends Component {
-  constructor(props){
-    super(props)
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       showLogin: true
-    }
+    };
   }
 
-  render(){
-    console.log('this redux', this.props)
+  render() {
+    console.log("this redux", this.props);
     return (
-      <div className='navbar-container'>
-        <div className='links-container'>
+      <div className="navbar-container">
+        <div className="links-container">
           <div>
             <NavLink exact to="/Listings" activeClassName="active">
               Cars For Sale
             </NavLink>
           </div>
           <div>
-            {this.props.user ? <NavLink exact to="/NewListing">Sell Your Car</NavLink>: alert("please login")}
+            {this.props.user ? (
+              <NavLink exact to="/NewListing">
+                Sell Your Car
+              </NavLink>
+            ) : (
+              alert("please login")
+            )}
+          </div>
 
-              
-            
+          <div>
+            {this.props.user ? (
+              <NavLink exact to="/Profile">
+                Profile
+              </NavLink>
+            ) : (
+              alert("please login")
+            )}
           </div>
         </div>
-  
-        <div className='login-container'>
+
+        <div className="login-container">
           <Login />
         </div>
       </div>
     );
   }
-  
 }
-
 
 function mapReduxToProps(reduxState) {
   return reduxState;
