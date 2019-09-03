0d4249b80ec50010844550e78b36b71c182f88f3
“Daniel” <daniellimhong@gmail.com>
2019-09-03 14:22:50 -0700

    view listing completed

--- src/Components/ViewListing/ViewListing.js
+++ src/Components/ViewListing/ViewListing.js
@@ -3,20 +3,31 @@ import axios from "axios";
 import { connect } from "react-redux"
 import { getUser } from "../../redux/reducer"
 
+
+
 class ViewListing extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      listing: []
+      listing: [],
+      userInfo: []
     };
   }
 
   async componentDidMount(){
       const listing = await axios
       .get(`/listings/get_listing/${this.props.match.params.id}`)
+      
       .then(res => {
-        //   console.log(res.data);
+          // console.log(res.data);
+          axios.get(`/listings/get_user_contact/${this.props.match.params.id}`)
+            .then(res => {
+              this.setState({
+                userInfo: res.data[0]
+              })
+              // console.log(res.data[0])
+            })
         return res.data
       })
       this.setState({
@@ -25,8 +36,7 @@ class ViewListing extends Component {
   }
 
   render() {
-      console.log(this.state.listing.car);
-      const { listing } = this.state
+      const { listing, userInfo } = this.state
 
     return (
     <div>
@@ -35,21 +45,27 @@ class ViewListing extends Component {
         <img src={listing.photos} alt="" />
         <h2>Price: {listing.price}</h2>
         <h2> Zip: {listing.zip} </h2>
-        {listing && listing.car && 
+        {listing && listing.car && userInfo && userInfo.username &&
         <div>
         <h2>Year: {listing.car.year} </h2>
           <h2>Make: {listing.car.make}</h2>
           <h2>Model: {listing.car.model}</h2>
           <h2>Trim: {listing.car.trim}</h2>
+          <h2>Seller: {userInfo.username}</h2>
+          <h2>Date Added: {listing.date_added}</h2>
           </div>
         }
-        <h2>Date Added: {listing.date_added}</h2>
         </div>
         ) : (
             <div></div>
         )}{" "}
-        <button>Contact Seller</button>
-        {/* //! Display Seller's ID */}
+        <button
+        onClick={() => {window.open(`mailto:${userInfo.email}`)}}
+        >Contact Seller</button>
+        {/* <Mailto email={userInfo.email}>
+          Contact Seller
+        </Mailto> */}
+        {/* //! Display Seller's Email */}
     </div>
     );
   }
