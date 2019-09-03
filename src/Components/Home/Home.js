753caf1af4632250b5d43296a4c832e715d3bfcd
iamnitinmisra <nitin.misra84@gmail.com>
2019-09-03 16:53:27 -0700

    update to featured

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import Featured from '../Featured/Featured'
 import './Home.scss'
 
 export default function Home(props){
     return(
         <div>
-            <NavBar />
+            <Featured />
         </div>
     )
 }
